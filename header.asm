33f1d96a5d8ed55f6d9f660f131b9ca0d0d209b5
Richard <zohassadar@gmail.com>
2024-02-12 10:12:04 -0700

    Update header

--- header.asm
+++ header.asm
@@ -1,6 +1,12 @@
-; todo, replace this with logic similar to CelestialAmber/TetrisNESDisasm
-
         .setcpu "6502"
 
-        .byte   $4E,$45,$53,$1A,$02,$02,$31,$00
-        .byte   $00,$00,$00,$00,$00,$00,$00,$00
+INES_MAPPER = 3 ; 3 = CNROM
+INES_MIRROR = 1 ; 0 = horizontal mirroring, 1 = vertical mirroring
+INES_SRAM   = 0 ; 1 = battery backed SRAM at $6000-7FFF
+
+.byte 'N', 'E', 'S', $1A ; ID
+.byte $02 ; 16k PRG chunk count
+.byte $02 ; 8k CHR chunk count
+.byte INES_MIRROR | (INES_SRAM << 1) | ((INES_MAPPER & $f) << 4)
+.byte (INES_MAPPER & %11110000)
+.byte $0, $0, $0, $0, $0, $0, $0, $0 ; padding
